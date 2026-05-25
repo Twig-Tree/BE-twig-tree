aa525ea97bfe4e94b43c8f6602672da5c27c8f95
Yujin Kim <yujii22001@gmail.com>
2026-05-25 23:31:30 +0900

    feat: swagger 연동

--- src/main/java/com/tree/twig_tree/domain/node/controller/NodeController.java
+++ src/main/java/com/tree/twig_tree/domain/node/controller/NodeController.java
@@ -1,4 +1,28 @@
 package com.tree.twig_tree.domain.node.controller;
 
+import com.tree.twig_tree.domain.node.service.NodeService;
+import com.tree.twig_tree.global.apiPayload.ApiResponse;
+import com.tree.twig_tree.global.apiPayload.code.BaseSuccessCode;
+import com.tree.twig_tree.global.apiPayload.code.GeneralSuccessCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class NodeController {
+
+    private final NodeService nodeService;
+
+    @GetMapping("/test")
+    public ApiResponse<String> test(
+            @RequestParam Long testId
+    ) {
+        BaseSuccessCode code = GeneralSuccessCode.OK;
+        return ApiResponse.onSuccess(code, nodeService.test());
+    }
+
+
+
 }
--- src/main/java/com/tree/twig_tree/domain/node/service/NodeService.java
+++ src/main/java/com/tree/twig_tree/domain/node/service/NodeService.java
@@ -1,4 +1,11 @@
 package com.tree.twig_tree.domain.node.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class NodeService {
+
+    public String test() {
+        return "테스트 결과입니다.";
+    }
 }
