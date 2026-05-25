9438c63531a2b7c6b0f1e440a8629a66a594d453
Yujin Kim <yujii22001@gmail.com>
2026-05-25 12:52:31 +0900

    init project

---
+++ src/main/java/com/tree/twig_tree/TwigTreeApplication.java
@@ -0,0 +1,13 @@
+package com.tree.twig_tree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwigTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwigTreeApplication.class, args);
+	}
+
+}
