5b8065136baf50b9f06c67ff06f96124a29d7ef2
Yujin Kim <127318207+sophia22001@users.noreply.github.com>
2026-05-26 14:14:07 +0900

    [Feat] 에러 핸들링, swagger 연동  (#6)
    * feat: 예외 처리 코드 작성
    
    * feat: 예외 처리 코드 작성
    
    * feat: swagger 연동
    
    * feat: controller advice 어노테이션 추가
    
    * feat: AI review 반영
    
    * feat: 예외 테스트 API 추가
    
    * feat: 예외 테스트 API 추가
    
    * feat: 포워딩 오류 리뷰 반영

--- src/main/java/com/tree/twig_tree/domain/node/controller/NodeController.java
+++ src/main/java/com/tree/twig_tree/domain/node/controller/NodeController.java
@@ -1,4 +1,33 @@
 package com.tree.twig_tree.domain.node.controller;
 
+import com.tree.twig_tree.domain.node.exception.NodeException;
+import com.tree.twig_tree.global.apiPayload.ApiResponse;
+import com.tree.twig_tree.global.apiPayload.code.BaseSuccessCode;
+import com.tree.twig_tree.global.apiPayload.code.GeneralErrorCode;
+import com.tree.twig_tree.global.apiPayload.code.GeneralSuccessCode;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class NodeController {
+
+    @GetMapping("/error1")
+    @Operation(summary = "에러 핸들러 동작 테스트 API", description = "flag 값이 false이면 의도적으로 NodeException을 발생시킵니다.")
+    public ApiResponse<String> testErrorHandler(@RequestParam(name = "flag") boolean flag) {
+
+        if (!flag) {
+            // 여기서 예외를 던지면 Controller Advice가 가로챕니다.
+            throw new NodeException(GeneralErrorCode.BAD_REQUEST);
+        }
+
+        BaseSuccessCode code = GeneralSuccessCode.OK;
+        return ApiResponse.onSuccess(code, "API가 성공적으로 호출되었습니다.");
+    }
+
+
+
 }
