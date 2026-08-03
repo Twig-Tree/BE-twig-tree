b1500724c45649aaa552762f681080f5e0e9b719
Yujin Kim <yujii22001@gmail.com>
2026-08-03 21:56:59 +0900

    feat: 길이 제한 복구. 다른 PR에서 해결할 예정

--- src/main/java/com/tree/twig_tree/domain/folder/dto/FolderReqDTO.java
+++ src/main/java/com/tree/twig_tree/domain/folder/dto/FolderReqDTO.java
@@ -2,13 +2,11 @@ package com.tree.twig_tree.domain.folder.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 public class FolderReqDTO {
 
     public record CreateFolder (
             @NotBlank
-            @Size(max = 15)
             String name,
 
             @Min(1) // null 가능
@@ -17,7 +15,6 @@ public class FolderReqDTO {
 
     public record UpdateFolder (
             @NotBlank
-            @Size(max = 15)
             String name
     ){}
 
