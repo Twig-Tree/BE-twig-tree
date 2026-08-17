e0fbcc67933416a94e94195ea63d71d3a4cb3ecb
Yujin Kim <yujii22001@gmail.com>
2026-08-17 17:14:15 +0900

    feat: 주석 수정

--- src/main/java/com/tree/twig_tree/global/common/entity/BaseEntity.java
+++ src/main/java/com/tree/twig_tree/global/common/entity/BaseEntity.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class) // 특정 이벤트(저장, 수정 등)가 발생했을 때, 이를 감지
 public abstract class BaseEntity {
 
-    @CreatedDate // 저장 이벤트가 발생 시 현재 시걱으로 저장
+    @CreatedDate // 저장 이벤트가 발생 시 현재 시각으로 저장
     @Column(nullable = false)
     private LocalDateTime createdAt;
 
