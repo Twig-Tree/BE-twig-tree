aa1ff80a76e3552a0185f7b2c28a76f6df62ad3d
Yujin Kim <yujii22001@gmail.com>
2026-08-18 14:51:09 +0900

    fix: getter 추가

--- src/main/java/com/tree/twig_tree/global/common/entity/BaseEntity.java
+++ src/main/java/com/tree/twig_tree/global/common/entity/BaseEntity.java
@@ -3,6 +3,7 @@ package com.tree.twig_tree.global.common.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class) // 특정 이벤트(저장, 수정 등)가 발생했을 때, 이를 감지
+@Getter
 public abstract class BaseEntity {
 
     @CreatedDate // 저장 이벤트가 발생 시 현재 시각으로 저장
