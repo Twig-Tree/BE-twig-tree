d6ba3dbecb676112b5f651b00fb9304526e49945
Yujin Kim <yujii22001@gmail.com>
2026-08-23 20:21:47 +0900

    feat: 용어 변경

--- src/main/java/com/tree/twig_tree/domain/node/exception/code/NodeErrorCode.java
+++ src/main/java/com/tree/twig_tree/domain/node/exception/code/NodeErrorCode.java
@@ -11,7 +11,7 @@ public enum NodeErrorCode implements BaseErrorCode {
 
     NODE_NOT_FOUND(HttpStatus.NOT_FOUND, "NODE404-1","해당 노드가 존재하지 않습니다."),
     PARENT_NOT_FOUND(HttpStatus.NOT_FOUND, "NODE404-2","부모 노드가 존재하지 않습니다."),
-    NODE_NOT_INCLUDED_IN_TREE(HttpStatus.BAD_REQUEST, "NODE400-1","해당 트리에 속하지 않은 노드입니다"),
+    NODE_NOT_IN_TREE(HttpStatus.FORBIDDEN, "NODE403-1","해당 트리에 속하지 않은 노드입니다"),
     DUPLICATED_ORDER_ID(HttpStatus.CONFLICT, "NODE409-1" , "같은 부모를 갖는 노드끼리는 order_id가 겹칠 수 없습니다."),
     ONE_ROOT_PER_TREE(HttpStatus.CONFLICT, "NODE409-2" , "하나의 트리에는 하나의 루트만 존재할 수 있습니다. ");
 
