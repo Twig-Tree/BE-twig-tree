7ee128d7a9f9c6c7d2c58de4258b73fc24931580
Yujin Kim <127318207+sophia22001@users.noreply.github.com>
2026-09-15 13:31:42 +0900

    fix: Instant로 시간 칼럼 변경 (#85)

--- src/main/java/com/tree/twig_tree/domain/workspace/dto/WorkspaceResDTO.java
+++ src/main/java/com/tree/twig_tree/domain/workspace/dto/WorkspaceResDTO.java
@@ -2,7 +2,7 @@ package com.tree.twig_tree.domain.workspace.dto;
 
 import lombok.Builder;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 
 public class WorkspaceResDTO {
 
@@ -17,6 +17,6 @@ public class WorkspaceResDTO {
             String name,
             Long folderId,
             Long treeId,
-            LocalDateTime updatedAt
+            Instant updatedAt
     ){}
 }
